162f30fa9818a540ae3e655b240b33bf1ad94710
李子豪 <15836784857@163.com>
2019-12-04 14:21:29 +0800

    vue2-editor测试

---
+++ Learn-Vue2-Editor/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="hello">
+        <vue-editor></vue-editor>
+    </div>
+</template>
+
+<script>
+    import {VueEditor} from "vue2-editor"
+
+    export default {
+        name: 'HelloWorld',
+        components: {
+            VueEditor
+        },
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+</style>
